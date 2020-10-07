3bd8cfc4871ac5f728afe730248b8a1775af7541
Mitchell John <mitchellvdhut@gmail.com>
2020-10-07 11:48:07 +0200

    added cors

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS
 from project import Project, Projects
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 api.add_resource(Projects, "/")
 api.add_resource(Project, "/project/<string:title>")
