541c4b7de76388ff89965aaa0e666a7cfda8d6b0
Mitchell John <mitchellvdhut@gmail.com>
2020-10-19 21:29:46 +0200

    routes refactor

--- app.py
+++ app.py
@@ -1,15 +1,41 @@
 from dotenv import load_dotenv
-from flask import Flask
-from project import get_projects
+from flask import Flask, request
+from project import get_projects, get_database, get_project, post_project, put_project, delete_project
+import sys, os
+
+if len(sys.argv) > 1:
+    var_value = sys.argv[1]
+else:
+    var_value = 'development'
+
+os.environ['FLASK_ENV'] = var_value
 
 load_dotenv('.env')
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
 
+projects = get_database(app)
 
-@app.route('/projects/')
+@app.route('/projects')
 def get_all_projects():
-    return get_projects(app)
+    return get_projects(app, projects)
+
+@app.route('/project', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def project():
+    if (request.method == 'GET'):
+        return get_project(projects)
+    if (request.method == 'POST'):
+        return post_project(projects)
+    if (request.method == 'PUT'):
+        return put_project(projects)
+    if (request.method == 'DELETE'):
+        return delete_project(projects)
+    return 'strange error man'
 
 
-app.run()
+print(os.environ.get('FLASK_ENV'))
+if __name__ == "__main__":
+    if ( os.environ.get('FLASK_ENV') == 'production' ):
+        app.run(host='0.0.0.0')
+    else:
+        app.run()
--- project.py
+++ project.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
-from flask import jsonify
-from bson import json_util
+from flask import jsonify, request, Response
+from flask_restful import Resource, reqparse
+from bson import json_util, ObjectId
 import json
 
 
@@ -12,9 +13,68 @@ def get_database(app):
     db = client.portfolio
     return db.projects
 
-
-def get_projects(app):
-    projects = get_database(app)
+def get_projects(app, projects):
     result = json.loads(json_util.dumps(projects.find()))
     return jsonify(result)
 
+def get_project(projects):
+    """"Return project with specific projectid."""
+    projectid = request.args.get('projectid')
+    result = json.loads(json_util.dumps(projects.find_one({"_id": ObjectId(projectid)})))
+    print(result)
+    return result
+
+def post_project(projects):
+    """Insert a document into the database."""
+    parser = reqparse.RequestParser()
+    parser.add_argument("title", required=True, help='Project title is required')
+    parser.add_argument("description", required=True, help='Project description is required')
+    parser.add_argument("image")
+    args = parser.parse_args()
+
+    print(args)
+    title = args["title"]
+
+    project = {
+        "title": args["title"],
+        "description": args["description"],
+        "image": args["image"]
+    }
+    
+    result=projects.insert_one(project)
+    
+    print('Created project {0} with id: {1}'.format(title,result.inserted_id))
+    return Response(status=201)
+
+def put_project(projects):
+    """Edit a database entry."""
+    parser = reqparse.RequestParser()
+    parser.add_argument("projectid", required=True, help='Project id is required')
+    parser.add_argument("title", required=True, help='Project title is required')
+    parser.add_argument("description", required=True, help='Project description is required')
+    parser.add_argument("image")
+    args = parser.parse_args()
+
+    projectid = args["projectid"]
+    filter = {"_id": ObjectId(projectid)}
+    project = { "$set": {
+        "title": args["title"],
+        "description": args["description"],
+        "image": args["image"]
+    }}
+    
+    projects.update_one(filter, project)
+    
+    print('Edited project with id: {0}'.format(projectid))
+    return Response(status=201)
+
+def delete_project(projects):
+    """Delete a database entry."""
+    parser = reqparse.RequestParser()
+    parser.add_argument("projectid", required=True, help='Project id is required')
+    args = parser.parse_args()
+
+    projectid = args["projectid"]
+
+    projects.delete_one({"_id": ObjectId(projectid)})
+    return Response("Post with id: {} is deleted.".format(projectid), status=200)
